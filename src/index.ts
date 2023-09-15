8ebc7083c397ac809b02413362cc31c7948ae9e0
Henrique Marques Louback <loubackhenrique123@gmail.com>
2023-09-14 21:37:30 -0300

    fine

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { users, products, createUser, createProduct, getAllUsers, getAllProducts, searchProductsByName} from './database';
-import { TProducts, TUsers } from './types'
-import express, { Request, Response} from 'express';
-import cors from 'cors';
+import {
+  users,
+  products,
+  createUser,
+  createProduct,
+  getAllUsers,
+  getAllProducts,
+  searchProductsByName,
+} from "./database";
+import { TProducts, TUsers } from "./types";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -9,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003");
+  console.log("Servidor rodando na porta 3003");
 });
 
 app.get("/ping", (req: Request, res: Response) => {
@@ -19,89 +27,90 @@ app.get("/ping", (req: Request, res: Response) => {
 // Endpoints GET
 
 app.get("/users", (req: Request, res: Response) => {
-    const allUsers: TUsers[] = users 
-    res.status(200).send(users)
-})
+  const allUsers: TUsers[] = users;
+  res.status(200).send(users);
+});
 
 app.get("/products", (req: Request, res: Response) => {
-    const allProducts: TProducts[] = products  
-    res.status(200).send(products)
-})
+  const allProducts: TProducts[] = products;
+  res.status(200).send(products);
+});
 
 // Endpoints POST
 
 app.post("/users", (req: Request, res: Response) => {
-    const { id, name, email, password }: TUsers = req.body
+  const { id, name, email, password }: TUsers = req.body;
 
-    // const newUser: TUsers = {
-    //     id,
-    //     name,
-    //     email,
-    //     password,
-    //     createdAt,
-    // }
-    const newUser = createUser(id, name, email, password)
+  // const newUser: TUsers = {
+  //     id,
+  //     name,
+  //     email,
+  //     password,
+  //     createdAt,
+  // }
+  const newUser = createUser(id, name, email, password);
 
-    res.status(201).send(newUser)
-})
+  res.status(201).send(newUser);
+});
 
 app.post("/products", (req: Request, res: Response) => {
-    const { id, name, price, description, imageUrl }: TProducts = req.body
+  const { id, name, price, description, imageUrl }: TProducts = req.body;
 
-    const newProduct: TProducts = {
-        id,
-        name,
-        price,
-        description,
-        imageUrl
-    }
-    products.push(newProduct)
+  const newProduct: TProducts = {
+    id,
+    name,
+    price,
+    description,
+    imageUrl,
+  };
+  products.push(newProduct);
 
-    res.status(201).send('Novo produto registrado com sucesso')
-})
+  res.status(201).send("Novo produto registrado com sucesso");
+});
 
 app.delete("/users/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    const indexToDelete = users.findIndex((user) => user.id === id)
+  const indexToDelete = users.findIndex((user) => user.id === id);
 
-    if(indexToDelete !== -1 ) {
-        users.splice(indexToDelete, 1)
-    } else {
-        console.log("Deu ruim, não encontrou o index para deletar");
-    }
+  if (indexToDelete !== -1) {
+    users.splice(indexToDelete, 1);
+  } else {
+    console.log("Deu ruim, não encontrou o index para deletar");
+  }
 
-    res.status(200).send("User deletado com sucesso!")
-})
+  res.status(200).send("User deletado com sucesso!");
+});
 
 app.delete("/products/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    const indexToDelete = products.findIndex((product) => product.id === id)
+  const indexToDelete = products.findIndex((product) => product.id === id);
 
-    if(indexToDelete !== -1 ) {
-        products.splice(indexToDelete, 1)
-    } else {
-        console.log("Deu ruim, não encontrou o index para deletar");
-    }
+  if (indexToDelete !== -1) {
+    products.splice(indexToDelete, 1);
+  } else {
+    console.log("Deu ruim, não encontrou o index para deletar");
+  }
 
-    res.status(200).send("Product deletado com sucesso!")
-})
+  res.status(200).send("Product deletado com sucesso!");
+});
 
 app.put("/products/:id", (req: Request, res: Response) => {
-    const id = req.params.id
-    const newName = req.body.name as string | undefined
-    const newPrice = req.body.price as number | undefined
-    const newDescription = req.body.description as string | undefined
-    const newImageUrl = req.body.imageUrl as string | undefined
+  const id = req.params.id;
+  const newName = req.body.name as string | undefined;
+  const newPrice = req.body.price as number | undefined;
+  const newDescription = req.body.description as string | undefined;
+  const newImageUrl = req.body.imageUrl as string | undefined;
 
-    const product = products.find((product) => product.id === id) 
+  const product = products.find((product) => product.id === id);
 
-    product.name = newName || product.name //este || é para não apagar as infos caso não sejam alteradas no json
-    product.price = newPrice || product.price
+  if (product) {
+    product.name = newName || product.name;
+    product.price = newPrice || product.price;
+    product.description = newDescription || product.description;
+    product.imageUrl = newImageUrl || product.imageUrl;
+  }
 
-    product.description = newDescription || product.description
-    product.imageUrl = newImageUrl || product.imageUrl
-
-    res.status(200).send("Produto atualizado com sucesso")
-})
+  res.status(200).send("Produto atualizado com sucesso");
+});
