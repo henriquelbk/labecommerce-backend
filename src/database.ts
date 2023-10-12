4973f572b50f8b42f4d90b7348d1a5f326c31764
Henrique Marques Louback <loubackhenrique123@gmail.com>
2023-10-12 17:52:18 -0300

    dureza

--- src/database.ts
+++ src/database.ts
@@ -1,35 +1,35 @@
-import { TProducts, TUsers } from './types'
+// import { TProducts, TUsers } from './types'
 
-export const users: TUsers[] = [
-    {
-        id: "u001",
-        name: "Fulano",
-        email: "fulano@email.com",
-        password: "fulano123",
-        createdAt: new Date().toISOString(),
-    },
-    {
-        id: "u002",
-        name: "Beltrano",
-        email: "beltrano@email.com",
-        password: "beltrano123",
-        createdAt: new Date().toISOString(),
-    }
-]
+// export const users: TUsers[] = [
+//     {
+//         id: "u001",
+//         name: "Fulano",
+//         email: "fulano@email.com",
+//         password: "fulano123",
+//         createdAt: new Date().toISOString(),
+//     },
+//     {
+//         id: "u002",
+//         name: "Beltrano",
+//         email: "beltrano@email.com",
+//         password: "beltrano123",
+//         createdAt: new Date().toISOString(),
+//     }
+// ]
 
-export const products: TProducts[] = [
-    {
-        id: "p001",
-        name: "foguete a023",
-        price: 35.000,
-        description: "foguete pequeno",
-        imageUrl: "https://picsum.photos/seed/Rocket/400",
-    },
-    {
-        id: "p002",
-        name: "foguete a024",
-        price: 85.000,
-        description: "foguete médio",
-        imageUrl: "https://picsum.photos/seed/Rocket/400",
-    }
-]
+// export const products: TProducts[] = [
+//     {
+//         id: "p001",
+//         name: "foguete a023",
+//         price: 35.000,
+//         description: "foguete pequeno",
+//         imageUrl: "https://picsum.photos/seed/Rocket/400",
+//     },
+//     {
+//         id: "p002",
+//         name: "foguete a024",
+//         price: 85.000,
+//         description: "foguete médio",
+//         imageUrl: "https://picsum.photos/seed/Rocket/400",
+//     }
+// ]
